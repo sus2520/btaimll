5037885a9afc5ac7a58755debb2092f2781e22ed
clouduser2025 <hussainfarheen915@gmail.com>
2025-05-20 21:08:52 +0530

    ASDFG

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 import './App.css';
 
-const API_URL = ' https://b628-216-81-245-98.ngrok-free.app';
+const API_URL = 'https://29f6-216-81-245-137.ngrok-free.app';
 
 function App() {
   const [chatSessions, setChatSessions] = useState([]); // List of chat sessions
